5822efe163b6563cde1024e162a7a51a2cdee853
Hyumaio <mayuyu4c@foxmail.com>
2018-03-05 16:47:56 +0800

    Chapter 2: selection sort

---
+++ code/02_selection_sort.py
@@ -0,0 +1,24 @@
+def findSmallest(arr):
+    """Find the smallest element in an array."""
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    """Sort an array from smallest to largest."""
+    newArr = list()
+    for i in range(len(arr)):
+        smallest_index = findSmallest(arr)
+        # Get smallest by smallest_index, remove it from the old array, then append it to the new array.
+        newArr.append(arr.pop(smallest_index))
+    return newArr
+
+
+arr = [43, 21, 24, 35, 81, 4, 27, 78, 71, 46, 31, 9]
+newArr = selectionSort(arr)
+print('After selection sort, the new array is', newArr)
