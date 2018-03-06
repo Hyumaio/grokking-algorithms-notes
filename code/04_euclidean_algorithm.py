3d9c42d9d8c28e3c97d18d2ae628ba10028dfecb
Hyumaio <mayuyu4c@gamil.com>
2018-03-06 22:12:59 +0800

    Optimize code 04_euclidean_algorithm

--- code/04_euclidean_algorithm.py
+++ code/04_euclidean_algorithm.py
@@ -1,8 +1,9 @@
-"""Euclidean Algorithm, also called Euclid's Algorithm, is a algorithm to solve the GCD(Greatest Common Divisor)/HCF(Highest Common Factor) of two numbers."""
+"""
+Euclidean Algorithm, also called Euclid's Algorithm, is a algorithm to solve the GCD(Greatest Common Divisor)/HCF(Highest Common Factor) of two numbers.
+"""
 
 
 def euclideanAlgorithm(numbers):
-    """"""
     foo, bar = numbers
     while True:
         remainder = foo % bar
@@ -12,21 +13,26 @@ def euclideanAlgorithm(numbers):
             foo, bar = bar, remainder
 
 
-numbers = (2000, 96)
-gcd = euclideanAlgorithm(numbers)
-print(gcd)
+if __name__ == '__main__':
+    numbers = (2000, 96)
+    gcd = euclideanAlgorithm(numbers)
+    print(gcd)
 
-# To get the GCD, there're two more solutions, I made a simple brief explanation down there.
+# =========================================================================================
+# To get the GCD, there're two more solutions, I made a simple brief explanation down here.
 #
 # numbers = (2000, 96)
-#
+# =============================
 # 1.Prime factor decomposition.
 # 8|2000  96
-#  2|250   12
-#     125   6
-# 125 and 6 is relatively prime, so the GCD of (2000, 96) is 8*2=16.
+# 2|250   12
+#   125   6
 #
+# Result: 125 and 6 is relatively prime, so the GCD of (2000, 96) is 8*2=16.
+# ==========================================================================
 # 2.Short division.
 # 2000 = 2*2*2*2*5*5*5
 # 96   = 2*2*2*2*2*3
-# The common part after equal sign is the GCD of (2000, 96), so its 2*2*2*2=16.
+#
+# Result: The common part after equal sign is the GCD of (2000, 96), so its 2*2*2*2=16.
+# =====================================================================================
