95c31717f92e5e2f7f9a13f3078449796f08e289
Hyumaio <mayuyu4c@gamil.com>
2018-03-07 08:57:08 +0800

    Optimize code 03

--- code/03_simple_recursive_function.py
+++ code/03_simple_recursive_function.py
@@ -2,8 +2,7 @@ def factorial(number):
     """A simple realization of the factorial uses recursion."""
     if number == 1:
         return 1
-    else:
-        return number * factorial(number - 1)
+    return number * factorial(number - 1)
 
 
 if __name__ == '__main__':
